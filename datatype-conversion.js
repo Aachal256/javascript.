d5c3b582a6a79e85f2a9331a7719f1ad53da3481
Aachal Thaware <129490535+Aachal256@users.noreply.github.com>
2024-05-11 18:25:26 +0530

    Add files via upload

---
+++ datatype-conversion.js
@@ -0,0 +1,48 @@
+let a="aachal";
+console.log(typeof a);
+console.log(typeof(a));
+
+let b=10;
+console.log(typeof b);
+
+let c=true;
+console.log(typeof c);
+
+let d=6.5;
+console.log(typeof d);
+
+let e=1
+let f=Boolean(e)
+console.log(f);
+
+
+/*  o/p-  
+
+string
+string
+number
+boolean
+number
+true
+
+
+*/
+
+
+
+
+//-- CONVERT ONE TO ANOTHER DATATYPES..
+
+let k='anaya';
+console.log(typeof k);
+
+let s=Number(k)
+console.log(typeof s);
+
+/*
+o/p-  //finally convert string to number in o/p- 
+
+string
+number
+
+*/
