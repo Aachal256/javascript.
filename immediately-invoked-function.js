592752f042ddf2466e792a0a0cc556d67ed2477d
Aachal Thaware <129490535+Aachal256@users.noreply.github.com>
2024-06-29 23:23:46 +0530

    Add files via upload

---
+++ immediately-invoked-function.js
@@ -0,0 +1,9 @@
+(function chai(){
+    console.log(' DB CONNECTED');
+})()  //O/P-   DB CONNECTED
+
+
+( (NAME) => {
+    console.log('DB CONNECTED TWO ${NAME}');
+
+})('AACHAL');  //DB CONNECTED TWO AACHAL 
