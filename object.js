c1991d2f9cfc3d15b26f56a7739914b9a3c0e71f
Aachal Thaware <129490535+Aachal256@users.noreply.github.com>
2024-07-31 21:14:09 +0530

    Add files via upload

--- object.js
+++ object.js
@@ -1,83 +1,29 @@
-//const mySym=symbol("key1")
-const jsUser={
-    name:"aachal",
-    age:22,
-    gmail:"aachal@google.com",
-    location:"bhopal",
-    //[mySym]:"mykey1",
-    isLoggedIn:false,
-    lastLoginday:["monday","saturday"]
-
+function multipleBy5(num){
+    return num*5
 }
-console.log(jsUser["name"]);
-console.log(jsUser["age"]);
-console.log(jsUser["gmail"]);
-console.log(jsUser["location"]);
-console.log(jsUser.mySym);
-
-/* o/p- aachal
-22
-aachal@google.com
-bhopal
+multipleBy5.power=2
+console.log(multipleBy5(5));
+console.log(multipleBy5.power);
+console.log(multipleBy5.prototype);
+/*
+o/p- 25
+2
+{}
 */
 
-console.log(jsUser.gmail);
-console.log(jsUser.name);
-console.log(jsUser.age);
-/*o/p-  aachal@google.com
-aachal
-22
-*/
-
-
-
-
-//Object.freeze(jsUser);
- /*
-o/p-  {
-  name: 'aachal',
-  age: 22,
-  gmail: 'aachal@google.com',
-  location: 'bhopal'
-
-
-
-jsUser.gmail="aachal@chatgpt.com"
-object.freeze(jsUser);
-
-console.log(jsUser.greeting());
-
-jsUser.greeting=function(){
-    console.log("hello js user");
+function createUser(username,score){
+    this.username=username
+    this.score=score
 }
-console.log(jsUser.greeting());
-
-*/
-const tinderUser={}
-tinderUser.id="1234abc",
-tinderUser.name='aachal',
-tinderUser.mail="chatGpt@gmail.com",
-console.log(tinderUser);
-//0/p-  { id: '1234abc', name: 'aachal', mail: 'chatGpt@gmail.com' }
-
-const obj1={1:"a",2:"b"}
-const obj2={3:"a",4:"b"}
-//const obj3={obj1,obj2}
-//const obj3 = object.assign(obj1,obj2);
-console.log(obj1,obj2);//{ id: '1234abc', name: 'aachal', mail: 'chatGpt@gmail.com' }
-
-const obj4={...obj1, ...obj2}
-console.log(obj4);/*{ '1': 'a', '2': 'b' } { '3': 'a', '4': 'b' }
-{ '1': 'a', '2': 'b', '3': 'a', '4': 'b' }*/
+createUser.prototype.increment=function(){
+    score++
+}
+createUser.prototype.printMe=function(){
+    console.log('score is ${this.score}');
+}
 
 
-//console.log(object.keys(tinderUser)); // entries,key,value is very imp object..
+const chai=createUser("chai",25)
+const tea=createUser("tea",250)
 
-console.log(tinderUser.hasOwnProperty('isLogged')); //o/p-  false
-
-
-{
-  {}
-  {}
-  {}
-}   //it is JSON 
+chai.printMe()
